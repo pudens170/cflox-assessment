36c269402203b3b2fbe958cb61d0085e2b97908f
Tomiwa <ayeni.tomiwa.pudens@gmail.com>
2021-04-30 13:20:10 +0100

    renamed the base package

---
+++ src/test/java/com/cflox/numberconverter/NumberConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cflox.numberconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
